2ed8e7e9e09c0fc2c213544621ccd35d2063a2d1
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-13 22:35:14 +0200

    add tailwind support for word Style

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,24 +5,32 @@ import React, { useContext } from 'react';
 
 interface IButton {
   type: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'secondary' | 'agree' | 'disagree';
+  variant?: 'primary' | 'secondary' | 'success' | 'danger';
   children?: React.ReactNode;
   disabled?: boolean;
 }
 const Button = ({ children, type, variant, disabled }: IButton) => {
   const { state, dispatch } = useContext(AppContext);
-  const baseClassName =
+  const baseStyles =
     'flex flex-row items-center justify-center rounded px-4 py-4 cursor-pointer focus:outline-none focus:ring text-xs sm:text-base';
 
-  const variantClassName =
+  const variantStyles = {
+    primary: 'bg-neutral-800 text-slate-50 hover:bg-cyan-500',
+    secondary:
+      'border border-neutral-800 bg-transparent text-gray-950 hover:bg-neutral-800 hover:text-slate-50',
+    success: 'bg-emerald-600 text-neutral-50 hover:bg-emerald-300 hover:text-neutral-900',
+    danger: 'bg-red-700 text-neutral-50 hover:bg-red-600 hover:text-neutral-50',
+  };
+
+  const variantStyle =
     variant === 'primary'
-      ? 'bg-neutral-800 text-slate-50 hover:bg-cyan-500'
+      ? variantStyles.primary
       : variant === 'secondary'
-      ? 'border border-neutral-800 bg-transparent text-gray-950 hover:bg-neutral-800 hover:text-slate-50'
-      : variant === 'agree'
-      ? 'bg-emerald-600 text-neutral-50 hover:bg-emerald-300 hover:text-neutral-900'
-      : variant === 'disagree'
-      ? 'bg-red-700 text-neutral-50 hover:bg-red-600 hover:text-neutral-50'
+      ? variantStyles.secondary
+      : variant === 'success'
+      ? variantStyles.success
+      : variant === 'danger'
+      ? variantStyles.danger
       : null;
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -35,7 +43,7 @@ const Button = ({ children, type, variant, disabled }: IButton) => {
         type={type}
         onClick={handleClick}
         disabled={disabled}
-        className={`${baseClassName} ${variantClassName}`}
+        className={`${baseStyles} ${variantStyle}`}
       >
         {children}
       </button>
