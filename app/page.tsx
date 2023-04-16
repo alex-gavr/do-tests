9bddb0e6dffb6c8c57f937ae1cb3e43ce97208aa
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-16 20:17:13 +0200

    add func to get data from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import MemoizedCommentSection from '@components/Comments/CommentSection';
 import SurveyContainer from './SurveyContainer';
 import CountDown from '@components/CountDown';
+import { surveyData } from '@db/db';
+import { InferModel } from 'drizzle-orm';
 
-const Home = () => {
+const Home = async () => {
+  const results = await surveyData;
+
+  
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-center gap-8 px-2 py-10 sm:px-4'>
       <div className='flex max-w-4xl flex-col items-center justify-center gap-4'>
@@ -13,7 +19,7 @@ const Home = () => {
           Пройдите этот БЕСПЛАТНЫЙ тест и узнайте, как вы можете зарабатывать деньги в Интернете.
         </p>
       </div>
-      <SurveyContainer />
+      <SurveyContainer results={results} />
       <MemoizedCommentSection />
       <CountDown />
     </main>
