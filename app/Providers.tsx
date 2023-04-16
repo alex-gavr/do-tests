7e746e2b63fe8d15e801ee2234d67ffc4205bc0b
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-16 20:34:11 +0200

    remove nonUnique

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,11 +1,9 @@
 'use client';
 import AutoExit from '@components/Monetization/AutoExit';
-import NonUnique from '@components/Monetization/NonUnique';
 import Reverse from '@components/Monetization/Reverse';
 import { AppProvider } from '@context/Context';
 import { AnimatePresence, LazyMotion } from 'framer-motion';
 import { PropsWithChildren } from 'react';
-// import { AppProvider } from '@/context/Context';
 
 const production = process.env.NODE_ENV === 'production';
 export function Providers({ children }: PropsWithChildren) {
@@ -13,7 +11,7 @@ export function Providers({ children }: PropsWithChildren) {
     <AppProvider>
       {production && <AutoExit />}
       {production && <Reverse />}
-      {production && <NonUnique />}
+      {/* {production && <NonUnique />} */}
       <LazyMotion features={async () => (await import('@utils/domAnimation')).default}>
         <AnimatePresence>{children}</AnimatePresence>
       </LazyMotion>
