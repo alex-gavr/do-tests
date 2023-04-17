6fc9acbf088eddfb8d4f14d054738092c8e13af6
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-17 13:05:03 +0200

    use 2 tables to get survey data. Questions/Answers

--- db/db.ts
+++ db/db.ts
@@ -1,8 +1,8 @@
 import { connect } from '@planetscale/database';
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { surveyAnswers, surveyQuestions } from './schema';
-import { IAnswer } from '@context/stateTypes';
-import { InferModel, eq, sql } from 'drizzle-orm';
+// import { IAnswer } from '@context/stateTypes';
+// import { eq, sql } from 'drizzle-orm';
 
 const connection = connect({
   host: process.env.DATABASE_HOST,
@@ -12,20 +12,24 @@ const connection = connect({
 
 const db = drizzle(connection);
 
-export const surveyData = db
-  .select({
-    id: surveyQuestions.id,
-    question: surveyQuestions.question,
-    answers: sql<IAnswer[]>`
-    JSON_ARRAYAGG(
-      JSON_OBJECT(
-        'id', ${surveyAnswers.id}, 
-        'text', ${surveyAnswers.answer}, 
-        'styleVariant', ${surveyAnswers.styleVariant}, 
-        'leadsToExit', ${surveyAnswers.leadsToExit}, 
-        'exits', ${surveyAnswers.exits}
-        ))`,
-  })
-  .from(surveyQuestions)
-  .leftJoin(surveyAnswers, eq(surveyQuestions.id, surveyAnswers.questionId))
-  .groupBy(surveyQuestions.id);
+// export const surveyData = db
+//   .select({
+//     id: surveyQuestions.id,
+//     question: surveyQuestions.question,
+//     answers: sql<IAnswer[]>`
+//     JSON_ARRAYAGG(
+//       JSON_OBJECT(
+//         'id', ${surveyAnswers.id}, 
+//         'text', ${surveyAnswers.answer}, 
+//         'styleVariant', ${surveyAnswers.styleVariant}, 
+//         'to', ${surveyAnswers.leadsTo}, 
+//         ))`,
+//   })
+//   .from(surveyQuestions)
+//   .leftJoin(surveyAnswers, eq(surveyQuestions.id, surveyAnswers.questionId))
+//   .groupBy(surveyQuestions.id);
+
+
+  export const surveyQuestions1 = db.select().from(surveyQuestions);
+
+  export const surveyAnswers1 = db.select().from(surveyAnswers);
