fb8881a50a4afb4f9b96f0bfd0f79b517474e9f2
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-22 19:42:54 +0200

    change db to india

--- db/db.ts
+++ db/db.ts
@@ -3,8 +3,8 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 
 const connection = connect({
   host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  username: process.env.DATABASE_USERNAME_MUMBAI,
+  password: process.env.DATABASE_PASSWORD_MUMBAI,
 });
 
 export const db = drizzle(connection);
