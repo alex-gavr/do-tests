2268181e0fbfcdc4661ad028fde9f3cad589494e
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-22 22:57:20 +0200

    remove middleware

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import TravelSurvey from './(travelSurvey)/TravelSurvey';
 
 const StartingPage = async ({ searchParams }: { searchParams: { [key: string]: string } }) => {
   const offerId = searchParams.offer_id ? parseInt(searchParams.offer_id) : undefined;
-  const country = searchParams.country ? searchParams.country : undefined;
+  // const country = searchParams.country ? searchParams.country : undefined;
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-center gap-8 px-2 pb-10 pt-20 sm:px-4'>
       {offerId === 9241 && <CareerSurvey />}
       {offerId === undefined && <DefaultSurvey />}
       {offerId === 9999 && <TravelSurvey />}
-      Welcome: {country}
+      {/* Welcome: {country} */}
     </main>
   );
 };
--- middleware22.ts
+++ middleware22.ts
@@ -7,6 +7,9 @@ export const config = {
 
 export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req;
+
+  // accept-language: 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+  
   const country = geo?.country || 'RU';
 
   url.searchParams.set('country', country);
