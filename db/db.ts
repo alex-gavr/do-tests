baebdaaf38934646933be48ded466e7b957f13a4
Alexander Gavrilenko <sashadrug@mail.ru>
2023-04-28 21:27:30 +0200

    change db to frankfurt

--- db/db.ts
+++ db/db.ts
@@ -3,8 +3,8 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 
 const connection = connect({
   host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME_SINGAPORE,
-  password: process.env.DATABASE_PASSWORD_SINGAPORE,
+  username: process.env.DATABASE_USERNAME_FRANKFURT,
+  password: process.env.DATABASE_PASSWORD_FRANKFURT,
 });
 
 export const db = drizzle(connection);
