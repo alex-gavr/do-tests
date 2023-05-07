a6f8573b8950b7144fbde0097e15284ddd108651
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-07 16:29:07 +0200

    remove push

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,12 @@ import '@styles/globals.css';
 import ProgressBar from '@components/ProgressBar';
 import { Analytics } from '@vercel/analytics/react';
 import Providers from './Providers';
-import Script from 'next/script';
 
 export const metadata = {
   title: 'Best offers',
   description: 'Give it a try',
   icons: {
     icon: '/favicon.ico',
-  },
-  other: {
-    pushsdk: '4b56e4173da530d3f2cdd7709ea8e218',
   },
 };
 
@@ -33,22 +29,6 @@ export default function RootLayout({ children }: IProps) {
         </body>
       </Providers>
       <Analytics />
-      <Script>
-        {
-          ` var s = document.createElement('script');
-            s.src='//deefauph.com/pfe/current/micro.tag.min.js?z=5931129'+'&sw=/sw-check-permissions-17b74.js';
-            s.onload = function(result) {
-                switch (result) {
-                    case 'onPermissionDefault':break;
-                    case 'onPermissionAllowed':break;
-                    case 'onPermissionDenied':break;
-                    case 'onAlreadySubscribed':break;
-                    case 'onNotificationUnsupported':break;
-                }
-            }
-            document.head.appendChild(s);`
-          }
-      </Script>
     </html>
   );
 }
