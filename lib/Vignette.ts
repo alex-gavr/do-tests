4ec12e72eaa42ebfcce7e7213335975fbc214060
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-14 20:54:56 +0200

    make secure call

--- lib/Vignette.ts
+++ lib/Vignette.ts
@@ -1,7 +1,7 @@
 export const Vignette = () => {
   const script = document.createElement('script');
   script.id = 'vignette';
-  script.src = `http://glizauvo.net/401/5948180`;
+  script.src = `https://glizauvo.net/401/5948180`;
   try {
     (document.body || document.documentElement).appendChild(script);
   } catch (e) {
