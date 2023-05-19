943c55409cf6d0ba9063bdbaeb704bbc7676519e
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-19 12:07:40 +0200

    same origin

--- utils/Vignette/getVignetteData.ts
+++ utils/Vignette/getVignetteData.ts
@@ -2,10 +2,12 @@ import { IVignette } from '@context/vignette/vignetteStateType';
 
 const getVignetteData = async (zone: string) => {
   try {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_VIGNETTE_URL}${zone}`,{credentials: 'same-origin'}).then((res) => {
-      console.log(res);
-      return res.json();
-    });
+    const data = await fetch(`${process.env.NEXT_PUBLIC_VIGNETTE_URL}${zone}`, { mode: 'same-origin' }).then(
+      (res) => {
+        console.log(res);
+        return res.json();
+      },
+    );
     // console.log(data);
     return data.ads[0] as IVignette;
   } catch (error) {
