075774e76a9e0cc0b01a9604ea2b343b89cdc641
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-19 11:30:07 +0200

    clg res

--- utils/Vignette/getVignetteData.ts
+++ utils/Vignette/getVignetteData.ts
@@ -2,8 +2,11 @@ import { IVignette } from '@context/vignette/vignetteStateType';
 
 const getVignetteData = async (zone: string) => {
   try {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_VIGNETTE_URL}${zone}`).then((res) => res.json());
-    console.log(data);
+    const data = await fetch(`${process.env.NEXT_PUBLIC_VIGNETTE_URL}${zone}`).then((res) => {
+      console.log(res);
+      return res.json();
+    });
+    // console.log(data);
     return data.ads[0] as IVignette;
   } catch (error) {
     console.log(error);
