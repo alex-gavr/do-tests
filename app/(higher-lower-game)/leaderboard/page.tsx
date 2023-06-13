98f8de73504ccf613fcb219062defc88e5c9ede0
Alexander Gavrilenko <sashadrug@mail.ru>
2023-06-13 20:15:22 +0200

    show user in leaderboard

--- app/(higher-lower-game)/leaderboard/page.tsx
+++ app/(higher-lower-game)/leaderboard/page.tsx
@@ -2,43 +2,51 @@ import PlayerCard from '@components/HigherLowerGameUi/PlayerCard';
 import { db } from '@db/connection';
 import { gameUser, leaderboardView } from '@db/schema';
 import { TrophyIcon } from '@heroicons/react/24/solid';
-import { desc, eq, inArray } from 'drizzle-orm';
+import { desc, eq, sql } from 'drizzle-orm';
 import { cookies } from 'next/dist/client/components/headers';
-import Link from 'next/link';
 import LeaderboardButtons from './LeaderboardButtons';
 import { getDictionary } from 'i18n';
 import { TValidLocale } from 'config';
 import { THigherLowerGameDictionary } from 'dictionaries/7777/en';
 import { IServerProps } from '@app/page';
 import { useServerSearchParams } from '@hooks/useServerSearchParams';
+import LeaderboardSuspense from '@components/HigherLowerGameUi/Skeletons/LeaderboardSuspense';
 
 const Page = async ({ searchParams }: IServerProps) => {
   const cookie = cookies();
   const playerName = cookie.get('playerName')?.value ?? '?????';
+  const playerId = cookie.get('playerId')?.value ?? '?????';
   const { language } = useServerSearchParams(searchParams);
 
-  const leaderboardDataReq = db
-    .select({ gameUser })
-    .from(leaderboardView)
-    .leftJoin(gameUser, eq(leaderboardView.uuid, gameUser.uuid))
-    .orderBy(desc(leaderboardView.topScore))
-    .limit(25);
+  const playerPlace = await db.select().from(leaderboardView).where(eq(leaderboardView.uuid, playerId));
 
-  const dReq = getDictionary(7777, language as TValidLocale);
+  if (playerPlace[0].place <= 10) {
+    const leaderboardDataReq = db
+      .select({ gameUser })
+      .from(leaderboardView)
+      .leftJoin(gameUser, eq(leaderboardView.uuid, gameUser.uuid))
+      .orderBy(desc(leaderboardView.topScore))
+      .limit(10);
 
-  const [leaderboardDataRes, dRes] = await Promise.all([leaderboardDataReq, dReq]);
-  // console.log('🚀 ~ leaderboardData:', leaderboardData)
-  const d = dRes as THigherLowerGameDictionary;
+    const dReq = getDictionary(7777, language as TValidLocale);
+    const totalPlayersReq = db.select({ players: sql<string>`COUNT(*)` }).from(leaderboardView);
 
-  if (leaderboardDataRes !== null) {
+    const [leaderboardDataRes, dRes, totalPlayersRes] = await Promise.all([
+      leaderboardDataReq,
+      dReq,
+      totalPlayersReq,
+    ]);
+
+    const d = dRes as THigherLowerGameDictionary;
+
     return (
       <div className='relative flex min-h-screen flex-col items-center gap-4 bg-slate-900 px-2 py-4'>
         <div className='flex flex-row items-center justify-center gap-2'>
-          <h1 className='text-center text-4xl text-cyan-300'>Leaderboard</h1>
+          <h1 className='text-center text-4xl text-cyan-300'>{d.leaderboard.heading}</h1>
           <TrophyIcon className='h-8 w-8 text-yellow-300' />
         </div>
-        <p className='text-center text-lg text-neutral-200'>TOP 25 Legends of the game</p>
-        <div className='mb-16 flex w-full flex-col items-center justify-center gap-2'>
+        <p className='text-center text-lg text-neutral-200'>{d.leaderboard.paragraph}</p>
+        <div className='flex w-full flex-col items-center justify-center gap-2'>
           {leaderboardDataRes.map((player, index) => {
             if (player.gameUser !== null) {
               return (
@@ -58,7 +66,81 @@ const Page = async ({ searchParams }: IServerProps) => {
             }
           })}
         </div>
-        {/* Button to go back, button to play again */}
+        <p className='mb-16 text-white'>
+          {d.leaderboard.totalPlayers} {totalPlayersRes[0].players}
+        </p>
+        <div className='fixed bottom-0 w-full max-w-2xl rounded-t-md bg-gray-600 bg-opacity-40 bg-clip-padding p-2 backdrop-blur-sm backdrop-filter'>
+          <LeaderboardButtons buttonsTexts={d.leaderboard.buttons} />
+        </div>
+      </div>
+    );
+  } else {
+    // SHOW THIS WHEN USER NOT IN A TOP 10
+    const leaderboardDataReq = db
+      .select({ gameUser: gameUser, place: leaderboardView.place })
+      .from(leaderboardView)
+      .leftJoin(gameUser, eq(leaderboardView.uuid, gameUser.uuid))
+      .orderBy(desc(leaderboardView.topScore))
+      .limit(10);
+
+    const playersReq = db
+      .select({ gameUser: gameUser, place: leaderboardView.place })
+      .from(gameUser)
+      .leftJoin(leaderboardView, eq(leaderboardView.uuid, playerId))
+      .where(eq(gameUser.uuid, playerId));
+
+    const dReq = getDictionary(7777, language as TValidLocale);
+    const totalPlayersReq = db.select({ players: sql<string>`COUNT(*)` }).from(leaderboardView);
+
+    const [leaderboardDataRes, dRes, totalPlayersRes, playerRes] = await Promise.all([
+      leaderboardDataReq,
+      dReq,
+      totalPlayersReq,
+      playersReq,
+    ]);
+
+    const d = dRes as THigherLowerGameDictionary;
+
+    return (
+      <div className='relative flex min-h-screen flex-col items-center gap-4 bg-slate-900 px-2 py-4'>
+        <div className='flex flex-row items-center justify-center gap-2'>
+          <h1 className='text-center text-4xl text-cyan-300'>{d.leaderboard.heading}</h1>
+          <TrophyIcon className='h-8 w-8 text-yellow-300' />
+        </div>
+        <p className='text-center text-lg text-neutral-200'>{d.leaderboard.paragraph}</p>
+        <div className='flex w-full flex-col items-center justify-center gap-2'>
+          {leaderboardDataRes.map((player, index) => {
+            if (player.gameUser !== null) {
+              return (
+                <PlayerCard
+                  place={index + 1}
+                  key={player.gameUser.uuid}
+                  name={player.gameUser.playerName}
+                  country={player.gameUser.country}
+                  highestScore={player.gameUser.topScore}
+                  hintsAvailable={player.gameUser.hintsAvailable}
+                  playerName={playerName}
+                  playerCardTexts={d.leaderboard.playerCard}
+                />
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+        <span className='text-white'>&#x2022; &#x2022; &#x2022;</span>
+        <PlayerCard
+          place={playerRes[0].place ? playerRes[0].place : 0}
+          name={playerRes[0].gameUser.playerName}
+          country={playerRes[0].gameUser.country}
+          highestScore={playerRes[0].gameUser.topScore}
+          hintsAvailable={playerRes[0].gameUser.hintsAvailable}
+          playerName={playerName}
+          playerCardTexts={d.leaderboard.playerCard}
+        />
+        <p className='mb-16 text-white'>
+          {d.leaderboard.totalPlayers} {totalPlayersRes[0].players}
+        </p>
         <div className='fixed bottom-0 w-full max-w-2xl rounded-t-md bg-gray-600 bg-opacity-40 bg-clip-padding p-2 backdrop-blur-sm backdrop-filter'>
           <LeaderboardButtons buttonsTexts={d.leaderboard.buttons} />
         </div>
