12095b92dcda530cd99aa3020fb29e0c11ff49ab
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-13 19:15:01 +0200

    provide geo from cookie, lazy load webVitals

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -5,7 +5,7 @@ import { AppProvider } from '@context/Context';
 import debug from '@utils/isDebug';
 import { useClientSearchParams } from '@hooks/useClientSearchParams';
 import exitZones from './(defaultSurvey)/Exits';
-import { WebVitals } from '@utils/WebVitals';
+// import WebVitals from '@utils/WebVitals';
 // import AutoExit from '@components/Monetization/AutoExit';
 // import InitPush from '@components/Monetization/InitPush';
 // import CookieChecker from '@components/CookiesChecker/CookiesChecker';
@@ -28,13 +28,16 @@ const CookieChecker = dynamic(() => import('@components/CookiesChecker/CookiesCh
 const InitPush = dynamic(() => import('@components/Monetization/InitPush'), {
   ssr: false,
 });
+const WebVitals = dynamic(() => import('@utils/WebVitals'), {
+  ssr: false,
+});
 
 interface IProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: IProps) => {
-  const { offerId, language, country } = useClientSearchParams();
+  const { offerId, language } = useClientSearchParams();
 
   const zone = offerId === 0 ? exitZones.push_zone[Math.floor(Math.random() * exitZones.push_zone.length)] : 5893057;
 
@@ -49,10 +52,8 @@ const Providers = ({ children }: IProps) => {
       <LazyMotion features={async () => (await import('@utils/domAnimation')).default}>
         <AnimatePresence>{children}</AnimatePresence>
       </LazyMotion>
-      {production && !debug && <WebVitals language={language} geo={country} offer={offerId} />}
+      {production && !debug && <WebVitals language={language} offer={offerId} />}
     </AppProvider>
   );
 };
 export default Providers;
-
-
--- utils/WebVitals.tsx
+++ utils/WebVitals.tsx
@@ -2,20 +2,27 @@
 import { useReportWebVitals } from 'next/web-vitals';
 import { Metric } from 'web-vitals';
 import sendToAnalyticsToDb from './sendToAnalyticsToDb';
+import { getCookie } from 'cookies-next';
 
 interface IProps {
   language: string;
-  geo: string;
   offer: number;
 }
 
-export function WebVitals({ language, geo, offer }: IProps) {
+const WebVitals = ({ language, offer }: IProps) => {
   //   onTTFB((metric) => sendToAnalyticsToDb(metric, pathname, language, geo, offer));
+
+  const geoCookie = getCookie('geo');
+  
   useReportWebVitals((metric: Metric) => {
     const url = new URL(window.location.href);
     const pathname = url.pathname;
+    const geo = geoCookie ? geoCookie.toString() : '??';
 
     sendToAnalyticsToDb({ metric, pathname, language, geo, offer });
   });
+
   return null;
 }
+
+export default WebVitals;
