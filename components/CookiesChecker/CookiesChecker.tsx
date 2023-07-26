9af93aecc0e57e774b285ee3aa978e44a9c4b0f6
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-26 15:03:21 +0200

    disable monetization of user with no cookies

--- components/CookiesChecker/CookiesChecker.tsx
+++ components/CookiesChecker/CookiesChecker.tsx
@@ -1,23 +1,16 @@
 'use client';
-import { useAppContext } from '@context/Context';
-import makeExitUrl, { ExitType } from '@utils/makeExitUrl';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const CookieChecker = () => {
   const router = useRouter();
-  const { surveyState } = useAppContext();
 
   useEffect(() => {
     const checkCookiesEnabled = () => {
       const cookieEnabled = navigator.cookieEnabled;
       if (!cookieEnabled) {
         console.log('Cookies are disabled');
-
-        // Redirect to ad
-        const url = makeExitUrl(surveyState.exits.cookiesDisabled, ExitType.onclick);
-        window.open(url, '_blank');
-        router.replace(url);
+        router.replace('https://google.com');
       } else {
         console.log('Cookies are enabled');
         // do nothing
